534709feb979ccdad35fecf68d2ea3da24db6737
codefirst2022 <codefirst2022@gmail.com>
2022-12-08 10:44:23 +0530

    post api call

--- src/app/components/api-get/api-get.component.ts
+++ src/app/components/api-get/api-get.component.ts
@@ -7,12 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./api-get.component.css']
 })
 export class ApiGetComponent implements OnInit {
-  candidates: any [] = [];
+  candidates: any[] = [];
+  selectedCandidate: string = '';
+  canidateObj: any = {
+    "CandidateId": 0,
+    "Name": "",
+    "ContactNo": "",
+    "Email": "",
+    "Password": "",
+    "CreatedDate": new Date(),
+    "City": "",
+    "CollegeName": "",
+    "Education": "",
+    "BankName": "",
+    "AccNo": "",
+    "IfscCode": "",
+    "Reference": ""
+  };
   constructor(private http: HttpClient) { }
   ngOnInit(): void {
-    this.http.get('https://onlinetestapi.gerasim.in/api/OnlineTest/GetAllCandidates').subscribe((response:any)=>{
+    this.loadCandiate();
+  }
+  loadCandiate() {
+    this.http.get('https://onlinetestapi.gerasim.in/api/OnlineTest/GetAllCandidates').subscribe((response: any) => {
       debugger;
       this.candidates = response.data;
+    })
+  }
+
+  onSaveCandidate() {
+    debugger
+    this.http.post('https://onlinetestapi.gerasim.in/api/OnlineTest/AddCandidate', this.canidateObj).subscribe((res: any) => {
+      debugger;
+      if (res.result) {
+        alert(res.message);
+        this.loadCandiate();
+      }
     })
   }
 }
